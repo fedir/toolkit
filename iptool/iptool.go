7621c83044cc44451e549477e525933bc48e6d3d
Fedir RYKHTIK <fedir@stratis.fr>
2019-03-13 10:57:35 +0100

    iptool > IP validator

---
+++ iptool/iptool.go
@@ -0,0 +1,39 @@
+package iptool
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fedir/toolkit/vartool"
+)
+
+// ValidateIP
+
+func ValidateIP(IPs interface{}, IP string, logging bool) bool {
+	var IPsSliced []string
+	var ipValidated bool
+	switch vartool.Type(IPs) {
+	case "string":
+		IPsSliced = strings.Split(IPs.(string), ",")
+	case "array":
+		IPsSliced = IPs.([]string)
+	default:
+		fmt.Println("IPs format is not recognized")
+		return false
+	}
+	for _, ip := range IPsSliced {
+		if ip == IP {
+			ipValidated = true
+
+		}
+	}
+	if logging == true {
+		switch ipValidated {
+		case true:
+			fmt.Println("IP Validated : ", IP)
+		default:
+			fmt.Println("IP NOT Validated : ", IP)
+		}
+	}
+	return ipValidated
+}
